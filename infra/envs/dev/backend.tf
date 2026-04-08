37dabaaa31adbf6c0a5d12e4ea3bcd3710a27662
ccittadino-matc <cabocittadino@gmail.com>
2026-04-07 21:14:44 -0500

    corrected backend configuration instructions

--- infra/envs/dev/backend.tf
+++ infra/envs/dev/backend.tf
@@ -4,7 +4,7 @@
 # 1. Copy `envs/dev/backend.tfbackend.example` to `envs/dev/backend.tfbackend`
 # 2. Update the values (bucket, region, dynamodb_table)
 # 3. Run:
-#      terraform init -backend-config=backend.tfbackend
+#      terraform init -backend-config="backend.tfbackend"
 #
 # This file only defines the backend type
 # The actual backend settings are supplied via the .tfbackend file
